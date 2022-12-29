fdadaed67a4ad19537a267357a06f011092d639b
EgboguJoseph <jegbogu@gmail.com>
2022-12-29 09:49:03 +0100

    update static props

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,49 @@
-import EventList from '../components/events/event-list'
+// import EventList from '../components/events/event-list'
 import {getFeaturedEvents}from '../dummy-data'
  
 
 
-const HomePage = () => {
-  const featuredEvents = getFeaturedEvents()
-  return ( 
-    <div>
+// const HomePage = () => {
+//   const featuredEvents = getFeaturedEvents()
+//   return ( 
+//     <div>
       
-      <EventList items={featuredEvents} />
+//       <EventList items={featuredEvents} />
       
-    </div>
-   );
-}
+//     </div>
+//    );
+// }
  
+// export default HomePage;
+import Head from 'next/head';
+
+// import { getFeaturedEvents } from '../helpers/api-util';
+import EventList from '../components/events/event-list';
+
+function HomePage(props) {
+  return (
+    <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name='description'
+          content='Find a lot of great events that allow you to evolve...'
+        />
+      </Head>
+      <EventList items={props.events} />
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+    revalidate: 1800,
+  };
+}
+
 export default HomePage;
